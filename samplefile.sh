403b35fe1b9a6942127afd8dace235961a0f50bf
janvish <visu04b8@gmail.com>
2022-06-22 07:43:00 +0530

    1st Stash Commit

---
+++ samplefile.sh
@@ -0,0 +1,6 @@
+Hi this is related to git stash concept 
+Stash is a temperory shelf or shelves changes you have made to your working directory copy(duplicate)
+so you can work on something else, and then come back and re-apply them later on.
+Stashing is handy if you need to quickly switch context and work on something else, but if you're in mid-way
+through a code change and arenot quickly ready to commit.
+
