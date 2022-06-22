ff9764b517039c293b204d6e8ab3fd6b4dbc9a49
janvish <visu04b8@gmail.com>
2022-06-22 08:14:48 +0530

    2nd stash commit

--- samplefile.sh
+++ samplefile.sh
@@ -1,4 +1,6 @@
 Hi this is related to git stash concept 
+
+1st changed in stash
 Stash is a temperory shelf or shelves changes you have made to your working directory copy(duplicate)
 so you can work on something else, and then come back and re-apply them later on.
 Stashing is handy if you need to quickly switch context and work on something else, but if you're in mid-way
